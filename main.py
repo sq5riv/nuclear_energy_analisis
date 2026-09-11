ca8abae4a6bd3fcf7d815134f532224765490a6f
Michał Kalenik <mchl.klnk@gmail.com>
2026-09-11 11:34:08 +0200

    Add clean data comun names methods

--- main.py
+++ main.py
@@ -7,5 +7,5 @@ if __name__ == "__main__":
     logger = get_logger(__name__)
     data_path = kaggle_utils.data_download(logger)
 #    docker_utils.docker_compose_up(logger)
-    print(before_load_data_check.colum_check(data_path, logger))
+    before_load_data_check.colum_check(data_path, logger)
 #    docker_utils.docker_down(logger)
--- python/before_load_data_check.py
+++ python/before_load_data_check.py
@@ -25,24 +25,40 @@ def get_csv_columns(csv_path: Path) -> list[str]:
     """Return column names from CSV."""
     with csv_path.open("r", encoding="utf-8-sig", newline="") as file:
         reader = csv.reader(file)
-        return next(reader)
+        names = [normalize_column_name(name) for name in next(reader)]
+        return names
 
 def get_documented_file(md_txt_path: Path) -> str | None:
     """Return CSV filename declared in MD."""
     content = md_txt_path.read_text(encoding="utf-8")
 
     match = re.findall(
-        r"`([^`]+?)(?<!\.csv)(?<!\.txt)(?<!\.pdf)(?<!\ )`",
+        r"(?:^|[-|\|])\s*`([^`]+?)(?<!\.csv)(?<!\.txt)(?<!\.pdf)(?<!\ )`",
         content
     )
+    match = [normalize_column_name(m) for m in match]
+    return list(set(match)) if match else None
 
-    return match if match else None
+def normalize_column_name(name: str) -> str:
+    name = re.sub(r"drililng", "drilling", name).strip()
+    return re.sub(r"\s+", " ", name).strip()
+
+def names_chceck(doc_cols: list, data_cols: list) -> bool:
+    """Return True if all column names match."""
+    return set(data_cols) == set(doc_cols)
 
 def colum_check(data_dir: str, logger: logging.Logger):
     """Check if all columns are present."""
     logger.info(f"Checking {data_dir} for columns")
 
-    csv_path, doc_path = get_file_pairs(data_dir, logger)[0]
-    #print(csv_path)
-    print(get_csv_columns(Path(csv_path)))
-    print(get_documented_file(Path(doc_path)))
+    for csv_path, doc_path in get_file_pairs(data_dir, logger):
+
+        data_cols = get_csv_columns(Path(csv_path))
+        doc_cols = get_documented_file(Path(doc_path))
+        if data_cols is None or doc_cols is None:
+            logger.info(f"There is wrong data in {csv_path} or {doc_path} file")
+        if not names_chceck(doc_cols, data_cols):
+            print(f'{doc_cols=}, {data_cols=}')
+            print(f'{set(doc_cols)- set(data_cols)}')
+            print(f'{set(data_cols)- set(doc_cols)}')
+        print(f'{names_chceck(doc_cols, data_cols)}, {csv_path=}')
